10836608aa56658988a841bfdaa18f57820fe6e3
UTULSA\aag781 <alvarogudis@gmail.com>
2018-03-29 09:55:00 -0500

    Basic dodge mechanic set up. Good example of basic autocancel thing

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 	public enum PlayerState {
-		DEFAULT
+		DEFAULT,
+        DODGESTART,
+        DODGING,
+        DODGEREC,
 	}
+
     //character states, in case we want to add stuff like a dash having invul, etc.
     public PlayerState charState = PlayerState.DEFAULT;
 
     public float speed;
+    public float statelock = 0;
+    public float dodgeTime = .3f;
 
     Rigidbody rigid;
     InputManager inputs;
@@ -28,12 +34,55 @@ public class PlayerControl : MonoBehaviour
     {
         switch(charState)
         {
-            case 0:
+            case PlayerState.DEFAULT:
                 Move();
+                break;
+            case PlayerState.DODGESTART:
+                DodgeInit();
+                break;
+            case PlayerState.DODGING:
+                Dodge();
+                break;
+            case PlayerState.DODGEREC:
+                DodgeRec();
                 break;
         }
 		
 	}
+
+    //Set up movement for dodge. 
+    void DodgeInit()
+    {
+        charState = PlayerState.DODGING;
+        statelock = dodgeTime;
+
+        //dodge to constant speed
+        if (rigid.velocity.magnitude > speed)
+        {
+            rigid.velocity = rigid.velocity.normalized * speed;
+        }
+        else
+        {
+            rigid.velocity = (new Vector3(inputs.GetAxis("Horizontal"), 0, inputs.GetAxis("Vertical"))).normalized * speed;
+        }
+    }
+
+    //dodge animation and state
+    void Dodge()
+    {
+        statelock -= Time.deltaTime;
+        if (statelock < 0f)
+        {
+            charState = PlayerState.DODGEREC;
+        }
+    }
+
+    //startup, recovery and dodge. If I were doing this in a project with a larger moveset, a spreadsheet would be doing the thinking here. Rn recover and start is 1f, but adding in those is pretty easy.
+    void DodgeRec()
+    {
+        rigid.velocity = Vector3.zero;
+        charState = PlayerState.DEFAULT;
+    }
 
     //Standard movement
     void Move()
@@ -45,6 +94,11 @@ public class PlayerControl : MonoBehaviour
         else
         {
             rigid.velocity = (new Vector3(inputs.GetAxis("Horizontal"), 0, inputs.GetAxis("Vertical"))) * speed;
+        }
+
+        if(Input.GetButtonDown("Dodge"))
+        {
+            charState = PlayerState.DODGESTART;
         }
     }
 }
