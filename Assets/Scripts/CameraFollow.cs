2d96c25e0ca4fa1af9207214b49f4493ad509bb5
Unknown <arichasting@gmail.com>
2018-03-29 10:42:34 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+
+	private float camY;
+
+	// Use this for initialization
+	void Start () {
+		camY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = player.position;
+		pos.y = camY;
+		transform.position = pos;
+	}
+}
