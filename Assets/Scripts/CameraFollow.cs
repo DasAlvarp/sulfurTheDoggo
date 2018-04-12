bc0530ba7d65be3862985c84ae9406105318a928
UTULSA\aag781 <alvarogudis@gmail.com>
2018-04-12 10:30:05 -0500

    Camera transitions

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,18 +6,75 @@ public class CameraFollow : MonoBehaviour {
 
 	public Transform player;
     public Transform laser;
+    public Camera cam;
 
 	private float camY;
+
+    private float minSize = 12;
+    private float maxSize = 20;
 
 	// Use this for initialization
 	void Start () {
 		camY = transform.position.y;
+        cam = transform.GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = (player.position * 3 + laser.position) / 4;
+        float magnitude = (player.position - laser.position).magnitude;
+        Vector3 pos;
+        if (magnitude > 8)
+        {
+            pos = (player.position * 3 + laser.position) / 4;
+        }
+        else if(magnitude > 2)
+        {
+            Vector3 pos1 = (player.position * 3 + laser.position) / 4;
+            Vector3 pos2 = (player.position);
+
+            float weight = (magnitude - 2) / 6 ;
+            pos = pos1 * weight + pos2 * (1 - weight);
+        }
+        else
+        {
+            pos = player.position;
+        }
+
 		pos.y = camY;
 		transform.position = pos;
+
+        float xDist = Mathf.Abs(player.position.x - laser.position.x);
+        float zDist = Mathf.Abs(player.position.z - laser.position.z);
+
+
+        float targetSize = minSize;
+
+        //if the camera is out of the range of effect
+        if(zDist > 8 || xDist > 20)
+        {
+            targetSize = Mathf.Max(zDist, xDist);
+
+            print("why");
+            //targetSize = Mathf.Sqrt((player.position - laser.position).magnitude) + minSize;
+            if(targetSize < minSize)
+            {
+                print("procx");
+                targetSize = minSize;
+            }
+        }
+        else
+        {
+            targetSize = minSize;
+        }
+
+
+        if(targetSize > cam.orthographicSize)
+        {
+            cam.orthographicSize += .2f;
+        }
+        if(targetSize < cam.orthographicSize)
+        {
+            cam.orthographicSize -= .2f;
+        }
 	}
 }
