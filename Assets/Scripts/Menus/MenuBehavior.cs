ff924d8d535e060f864cb793ad53f1020f1d4099
UTULSA\aag781 <alvarogudis@gmail.com>
2018-04-17 10:18:36 -0500

    More features to load scenes from

--- Assets/Scripts/Menus/MenuBehavior.cs
+++ Assets/Scripts/Menus/MenuBehavior.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuBehavior : MonoBehaviour {
+/*
+ * Created 4-3-18 by Alvaro Gudiswitz
+ * Easy script for buttons to jump between scenes
+ * Updates 4-17-18 by Alvaro Gudiswitz
+ */
+
+public class MenuBehavior : MonoBehaviour
+{
 
     public void LoadByIndex(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void LoadByName(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 }
