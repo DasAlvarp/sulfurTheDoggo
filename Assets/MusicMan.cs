13f3d4836cd8a0b3aaa0f3d042645a216ba08dc7
UTULSA\aag781 <alvarogudis@gmail.com>
2018-05-01 19:16:31 -0500

    bum bade (dumba bumba dumba ba bunmba0 ding dindg dong I A M ON M ASTER AND I DON'T GIVE A FUCK

---
+++ Assets/MusicMan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MusicMan : MonoBehaviour {
+    public AudioMixer audioGroup;
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+        if(GameObject.FindGameObjectWithTag("Singleton"))
+        {
+            if(gameObject != GameObject.FindGameObjectWithTag("Singleton"))
+            {
+                Destroy(gameObject);
+            }
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(SceneManager.GetActiveScene().name);
+		if(SceneManager.GetActiveScene().name != "javier test")
+        {
+            audioGroup.TransitionToSnapshots(new AudioMixerSnapshot[] { audioGroup.FindSnapshot("faded")}, new float[]{ 1f}, 0f);
+        }
+        else
+        {
+            audioGroup.TransitionToSnapshots(new AudioMixerSnapshot[] { audioGroup.FindSnapshot("normal") }, new float[] { 1f }, 0f);
+        }
+    }
+}
